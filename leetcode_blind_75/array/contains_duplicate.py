5df872ad00bfa1119958522648a42cf674ddc88a
poeticcode01 <cmahto01@gmail.com>
2024-02-10 18:36:40 +0530

    contains duplicate

---
+++ leetcode_blind_75/array/contains_duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        element_set = set()
+        for itm in nums:
+            if itm in element_set:
+                return True
+            element_set.add(itm)
+        return False
