ba08c06d3d79d2399b67831900c4dc8b70793460
poeticcode01 <cmahto01@gmail.com>
2024-03-11 22:32:56 +0530

    enhanced condition to handle cases more gracefully

--- leetcode_blind_75/interval/insert_interval.py
+++ leetcode_blind_75/interval/insert_interval.py
@@ -5,23 +5,22 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         if not intervals:
             return [newInterval]
-        ans = []
-        # if len(intervals) == 1  :
-        #     cur_interval = intervals[0]
-        #     if cur_interval[0] < 
-        #     ans.append([min(cur_interval[0],newInterval[0]),max(cur_interval[1],newInterval[1])])
-        #     return ans
-            
+        if newInterval[1] < intervals[0][0]:
+            ans = [newInterval]+intervals
+            return ans
+        if newInterval[0] > intervals[-1][1]:
+            intervals.append(newInterval)
+            return intervals
+
+
         ans = []
         ind = 0
         flag = False
         while ind  < len(intervals):
             cur_interval = intervals[ind]
-            nxt = None
-            if ind + 1 < len(intervals):
-                nxt = intervals[ind+1]
             if  cur_interval[1] < newInterval[0]:
                 ans.append(cur_interval)
+
             elif not flag and cur_interval[0] > newInterval[0]:
                 flag = True
                 min_itm = newInterval[0]
@@ -36,9 +35,7 @@ class Solution:
                         ans.append([min_itm,newInterval[1]])
                         ans.append(intervals[ind])
 
-            
-
-            elif cur_interval[0] <= newInterval[0] and nxt and nxt[0] > newInterval[0]:
+            elif cur_interval[0] <= newInterval[0] and  cur_interval[1] >= newInterval[0]:
                 flag = True
                 if newInterval[1] < cur_interval[1]:
                     ans.append(cur_interval)
@@ -56,13 +53,8 @@ class Solution:
                         else:
                             ans.append([min_itm,newInterval[1]])
                             ans.append(intervals[ind])
-            elif not nxt and cur_interval[0] <= newInterval[0] and cur_interval[1] >= newInterval[0]:
-                flag = True
-                ans.append([min(cur_interval[0],newInterval[0]),max(cur_interval[1],newInterval[1])])
-            
             else: 
                 ans.append(cur_interval)
             ind +=1
-        if not flag:
-            ans.append(newInterval)
+
         return ans
