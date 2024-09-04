ec0238fa08e7914b46437093d95674bc2636fa33
poeticcode01 <cmahto01@gmail.com>
2024-09-04 20:12:18 +0530

    merge interval

--- leetcode_blind_75/interval/merge_interval.py
+++ leetcode_blind_75/interval/merge_interval.py
@@ -3,26 +3,20 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key = lambda x: x[0])
-        ref_interval = intervals[0]
-        ind = 1
+        intervals = sorted(intervals)
+        strt = intervals[0][0]
+        end = intervals[0][1]
+
         ans = []
-        flag = False
-        while ind < len(intervals):
-            cur_interval = intervals[ind]
-            max_end = ref_interval[1]
-            while  ind < len(intervals) and cur_interval[0] <= max_end:
-                max_end = max(cur_interval[1],max_end)
-                ind +=1
-                if ind < len(intervals):
-                    cur_interval = intervals[ind]
-            if ind == len(intervals):
-                ans.append([ref_interval[0],max_end])
-                return ans
+        i = 1
+        while i < len(intervals):
+            if intervals[i][0] <= end:
+                end = max(end,intervals[i][1])
             else:
-                flag = True
-                ans.append([ref_interval[0],max_end])
-                ref_interval = intervals[ind]
-            ind +1
-        
-        return ans if flag else intervals
+                ans.append([strt,end])
+                strt = intervals[i][0]
+                end = intervals[i][1]
+            i +=1
+
+        ans.append([strt,end])
+        return ans
