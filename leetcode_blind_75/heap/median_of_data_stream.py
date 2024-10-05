4c8e81eb4f3e815b4a8e453cc0b3fa7951d1490e
poeticcode01 <cmahto01@gmail.com>
2024-10-05 21:20:15 +0530

    median of data stream

--- leetcode_blind_75/heap/median_of_data_stream.py
+++ leetcode_blind_75/heap/median_of_data_stream.py
@@ -2,46 +2,37 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.min_heap = []
         self.max_heap = []
-        
-
+        self.min_heap = []
+      
     def addNum(self, num: int) -> None:
-        # print(num,"min_heap",self.min_heap,"max_heap", self.max_heap)
         heapq.heappush(self.max_heap,-num)
-        if (len(self.max_heap) - len(self.min_heap) > 1):
+        # print(num)
+
+        while self.max_heap and len(self.max_heap) - len(self.min_heap) > 1:
             k = heapq.heappop(self.max_heap)
             heapq.heappush(self.min_heap,-k)
 
-        while len(self.max_heap) and len(self.min_heap) and (-self.max_heap[0]) > self.min_heap[0]:
+        while self.max_heap and self.min_heap and -self.max_heap[0] > self.min_heap[0]:
             k = heapq.heappop(self.max_heap)
             heapq.heappush(self.min_heap,-k)
         
-        while len(self.min_heap) > len(self.max_heap):
+        while self.min_heap and len(self.min_heap) > len(self.max_heap):
             k = heapq.heappop(self.min_heap)
             heapq.heappush(self.max_heap,-k)
 
-        # print(num,"min_heap",self.min_heap,"max_heap", self.max_heap)
+        
+        # print(num,self.max_heap,self.min_heap)
         
 
     def findMedian(self) -> float:
-        # print("find median" ,"min_heap",self.min_heap,"max_heap", self.max_heap)
-        min_heap_size = len(self.min_heap)
-        max_heap_size = len(self.max_heap)
-        if min_heap_size == 0 and max_heap_size == 0:
-            # print(1)
-            return None
+        if not self.max_heap:
+            return self.min_heap[0]
+        
+        if not self.min_heap:
+            return -self.max_heap[0]
+
+        if len(self.max_heap) == len(self.min_heap):
+            return (-self.max_heap[0] + self.min_heap[0])/2
         else:
-            # print(2)
-            if min_heap_size == 0:
-                return -self.max_heap[0]
-            if max_heap_size == 0:
-                return self.min_heap[0]
-            
-            if min_heap_size == max_heap_size:
-                return (self.min_heap[0] - self.max_heap[0])/2
-            else:
-                # if len(self.min_heap) > len(self.max_heap):
-                #     return self.min_heap[0]
-                # else:
-                return -self.max_heap[0]
+            return -self.max_heap[0]
