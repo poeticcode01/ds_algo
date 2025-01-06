166732652ca9dd66bdf285840b388e761f23b233
poeticcode01 <cmahto01@gmail.com>
2025-01-07 00:11:06 +0530

    fixed edge cases

--- leetcode_blind_75/backtrack/combination_sum.py
+++ leetcode_blind_75/backtrack/combination_sum.py
@@ -4,23 +4,32 @@ from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         self.ans = set()
-        self.getCandidateSum(candidates, "", 0,target)
-        
-            
-        return list(self.ans)
+        candidates = sorted(candidates)
+        self.getCandidateSum(candidates, [], target)
+        self.final_ans = []
+        for ans_str in self.ans:
+            temp = ans_str.split(",")
+            temp = [int(num) for num in temp]
+            self.final_ans.append(temp)
 
-    def getCandidateSum(self,candidates, run_str, run_sum, target):
-        
+        return self.final_ans
+
+    def getCandidateSum(self,candidates, run_list, target):
+        cur_sum = 0
+        if len(run_list) > 0:
+            cur_sum = sum(run_list)
 
         for cur_val in candidates:
-            temp = sorted(run_str+str(cur_val))
-            if cur_val + run_sum == target:
+            temp1 = sorted(run_list+[cur_val])
+            temp2 = [str(num) for num in temp1]
+            temp3 = ",".join(temp2)
+            if cur_val + cur_sum == target:
                 
-                if temp  not in  self.ans:
-                    self.ans.append(temp)
+                if temp3  not in  self.ans:
+                    self.ans.add(temp3)
                
                 return
-            elif cur_val + run_sum < target:
-                self.getCandidateSum(candidates, temp, cur_val + run_sum, target)
+            elif cur_val + cur_sum < target:
+                self.getCandidateSum(candidates, temp1, target)
             else:
                 return
