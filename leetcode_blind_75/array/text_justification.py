e9b09abb607c0cc7787c0ec05751863ea5e7d9d9
poeticcode01 <cmahto01@gmail.com>
2025-01-24 22:41:26 +0530

    text justification

--- leetcode_blind_75/array/text_justification.py
+++ leetcode_blind_75/array/text_justification.py
@@ -3,93 +3,101 @@ from typing import List
 
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        i = 0
+        
+        
+        self.word_len = len(words)
+        self.words = words
+        self.maxWidth = maxWidth
+
+        wordsPerline = self.packWordsPerline()
+        
+
+        self.final_ans = []
+        for ind,cur_line in enumerate(wordsPerline):
+            # print(ind,cur_line)
+            each_space_width,rem_space_width = self.getSpaceWidthPerSlot(cur_line)
+    
+            if ind+1 == len(wordsPerline):
+                self.handleLastLine(cur_line)   
+            else:
+                self.hanldeNonLastLine(cur_line,each_space_width,rem_space_width)
+                
+
+            
+        return self.final_ans
+    def packWordsPerline(self) -> List[str]:
         ans = []
-        word_len = len(words)
-        while i < word_len:
+        i = 0
+        while i < self.word_len:
             cur_width = 0
-            j = i
             cur_word_list = []
-            while j < word_len and cur_width < maxWidth:
-                temp_len = len(words[j])
+            while i < self.word_len and cur_width < self.maxWidth:
+                temp_len = len(self.words[i])
                 if cur_width == 0:
-                    cur_word_list.append(words[j])
+                    cur_word_list.append(self.words[i])
                     cur_width += temp_len
-                    j +=1
+                    i +=1
                     continue
                 else:
-                    if cur_width + temp_len + 1 <= maxWidth:
-                        cur_word_list.append(words[j])
+                    if cur_width + temp_len + 1 <= self.maxWidth:
+                        cur_word_list.append(self.words[i])
                         cur_width += (temp_len+1)
-                        j +=1
+                        i +=1
                         continue
                     else:
                         break
             ans.append(cur_word_list)
-            i = j
+        return ans
+    
+    def getSpaceWidthPerSlot(self,cur_line) ->(int,int): # type: ignore
+        space_slot = len(cur_line) - 1
+        word_width = 0
+        for word in cur_line:
+            word_width += len(word)
 
-        final_ans = []
-        for ind,cur_line in enumerate(ans):
-            # print(ind,cur_line)
-            space_slot = len(cur_line)-1
-            word_width = 0
+        total_space_width = self.maxWidth - word_width
+        if space_slot >= 1:
+            each_space_width = total_space_width//space_slot
+            rem_space_width = total_space_width%space_slot
+        else:
+            each_space_width = 0
+            rem_space_width = total_space_width
+
+        return each_space_width,rem_space_width
+
+    def handleLastLine(self,cur_line):
+        temp = ""
+        for word in cur_line:
+            if temp == "":
+                temp +=word
+            else:
+                temp = temp + " " + word
+        rem_space = self.maxWidth - len(temp)
+        temp = temp + " "*rem_space
+        self.final_ans.append(temp)
+
+    def hanldeNonLastLine(self,cur_line,each_space_width,rem_space_width):
+        each_space_slot = " "*each_space_width
+        if rem_space_width == 0:
+            temp = ""
             for word in cur_line:
-                word_width += len(word)
-
-            total_space_width = maxWidth - word_width
-            if space_slot >= 1:
-                each_space_width = total_space_width//space_slot
-                rem_space_width = total_space_width%space_slot
-            else:
-                each_space_width = 0
-                rem_space_width = total_space_width
-
-
-
-            each_space_slot = " "*each_space_width
-            if rem_space_width == 0:
-                if ind + 1  == len(ans):
-                    # print("last_line",cur_line)
-                    temp = ""
-                    for word in cur_line:
-                        if temp == "":
-                            temp +=word
-                        else:
-                            temp = temp + " " + word
-                    rem_space = maxWidth - len(temp)
-                    temp = temp + " "*rem_space
-                    final_ans.append(temp)
+                if temp == "":
+                    temp +=word
                 else:
-                    temp = ""
-                    for word in cur_line:
-                        if temp == "":
-                            temp +=word
-                        else:
-                            temp = temp + each_space_slot + word
-                    final_ans.append(temp)
-            else:
-                if ind + 1  == len(ans):
-                    # print("last_line",cur_line)
-                    temp = ""
-                    for word in cur_line:
-                        if temp == "":
-                            temp +=word
-                        else:
-                            temp = temp + " " + word
-                    rem_space = maxWidth - len(temp)
-                    temp = temp + " "*rem_space
-                    final_ans.append(temp)
+                    temp = temp + each_space_slot + word
+            self.final_ans.append(temp)
+        else:
+            temp = ""
+            for word in cur_line:
+                if temp == "":
+                    temp +=word
                 else:
-                    temp = ""
-                    for word in cur_line:
-                        if temp == "":
-                            temp +=word
-                        else:
-                            if rem_space_width > 0:
-                                temp = temp + each_space_slot + " " + word
-                                rem_space_width -=1
-                            else:
-                                temp = temp + each_space_slot +  word
-                    temp = temp + " "*rem_space_width
-                    final_ans.append(temp)
-        return final_ans
+                    if rem_space_width > 0:
+                        temp = temp + each_space_slot + " " + word
+                        rem_space_width -=1
+                    else:
+                        temp = temp + each_space_slot +  word
+            temp = temp + " "*rem_space_width
+            self.final_ans.append(temp)
+
+    
