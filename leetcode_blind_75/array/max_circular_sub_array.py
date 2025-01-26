fd1f8ef1e2661a964b59d583563d8ecdb724664d
poeticcode01 <cmahto01@gmail.com>
2025-01-26 21:20:17 +0530

    max circular sub array

--- leetcode_blind_75/array/max_circular_sub_array.py
+++ leetcode_blind_75/array/max_circular_sub_array.py
@@ -1,20 +1,52 @@
 from typing import List
 
-
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        strt = 0
+
         n = len(nums)
         if n == 1:
             return nums[0]
-        end = 1
-        cur_max_sum = nums[strt]
-        max_glob_sum = nums[strt]
+        
 
+        total_sum = sum(nums)
+        max_glob_sum = self.get_global_max(nums)
+        if max_glob_sum < 0:
+            return max_glob_sum
+
+        min_glob_sum = self.get_global_min(nums)
+        
+        return max((total_sum - min_glob_sum),max_glob_sum)
+
+    def get_global_min(self,nums):
+        strt = 0
+        end = 1
+
+        n = len(nums)      
         cur_min_sum = nums[strt]
         min_glob_sum = nums[strt]
 
-        total_sum = sum(nums)
+        while end < n:
+            temp = cur_min_sum + nums[end]
+            if temp <= nums[end]:
+                cur_min_sum= temp
+            else:
+                strt = end
+                cur_min_sum = nums[end]
+            
+            min_glob_sum = min(cur_min_sum ,min_glob_sum)
+
+            end +=1
+        return min_glob_sum
+
+    def get_global_max(self,nums):
+        strt = 0
+        end = 1
+
+        n = len(nums)
+
+        
+        cur_max_sum = nums[strt]
+        max_glob_sum = nums[strt]
 
         while end < n:
             temp = cur_max_sum + nums[end]
@@ -27,20 +59,4 @@ class Solution:
             max_glob_sum = max(cur_max_sum ,max_glob_sum)
 
             end +=1
-        end = 1
-        while end < n:
-            temp = cur_min_sum + nums[end]
-            if temp <= nums[end]:
-                cur_min_sum= temp
-            else:
-                strt = end
-                cur_min_sum = nums[end]
-            
-            min_glob_sum = min(cur_min_sum ,min_glob_sum)
-
-            end +=1
-
-        if max_glob_sum < 0:
-            return max_glob_sum
-        # print(total_sum,min_glob_sum,max_glob_sum)
-        return max((total_sum - min_glob_sum),max_glob_sum)
+        return max_glob_sum
