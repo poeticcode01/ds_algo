2f728faaf16451345411aabe5baf85d11a931b0f
poeticcode01 <cmahto01@gmail.com>
2025-02-01 22:48:27 +0530

    code refactoring

--- leetcode_blind_75/linkedlist/lru.py
+++ leetcode_blind_75/linkedlist/lru.py
@@ -18,84 +18,59 @@ class LRUCache:
     def get(self, key: int) -> int:
         if self.map.get(key) is None:
             return -1
-        # print(key,"tail",self.tail.key)
+
         temp_node = self.map[key]
         if self.head == temp_node:
             return temp_node.val
-        elif self.tail == temp_node:
-            prv = temp_node.prev
-            nxt = temp_node.next
-            if prv:
-                prv.next = None
-                self.tail = prv
-                # print("prev",prv.key,prv.val)
-                # print(key,"tail",self.tail.key)
-            if nxt:
-                nxt.prev = prv
-
-            temp_node.prev = None
-            temp_node.next  = self.head
-            self.head.prev = temp_node
-            temp_node.next = self.head
-            self.head = temp_node
         else:
             prv = temp_node.prev
             nxt = temp_node.next
             if prv:
-                prv.next = nxt
+                if self.tail == temp_node:
+                    self.update_tail(prv)
+                else:
+                    prv.next = nxt         
             if nxt:
                 nxt.prev = prv
             
             temp_node.prev = None
-            temp_node.next  = self.head
-            self.head.prev = temp_node
-            temp_node.next = self.head
-            self.head = temp_node
-        # self.map[key] = self.head
+            self.update_head(temp_node)     
+        
         return temp_node.val
+
+    def update_tail(self,prv):
+        prv.next = None
+        self.tail = prv
+    
+
+    def update_head(self,new_node):
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
             
-
-
-
         
 
     def put(self, key: int, value: int) -> None:
-        # print(key,value,self.size)
         if self.map.get(key) is None:
+            new_node = Node(key,value)
             if self.size == self.capacity:
-            
                 if self.capacity == 1:
-                    del self.map[self.head.key]
-                    new_node = Node(key,value)
+                    del self.map[self.head.key]    
                     self.head = new_node
-                    self.tail = new_node
-                    
+                    self.tail = new_node   
                 else:
-                    # print(key,value)
-                    # print(self.tail.key,self.tail.val)
                     del self.map[self.tail.key]
-                    temp = self.tail.prev
-                    temp.next = None
-                    self.tail = temp
-
-                    new_node = Node(key,value)
-                    new_node.next = self.head
-                    self.head.prev = new_node
-                    self.head = new_node
+                    prv = self.tail.prev
+                    self.update_tail(prv)
+                    self.update_head(new_node)
             else:
                 self.size +=1
                 if self.head:
-                    new_node = Node(key,value)
-                    new_node.next = self.head
-                    self.head.prev = new_node
-                    self.head = new_node
-                    # print(self.head.key,self.head.val)
+                    self.update_head(new_node)     
                 else:
-                    new_node = Node(key,value)
                     self.head = new_node
                     self.tail = new_node
-        else:
-            
+        else:   
             temp_node = self.map[key]
             temp_node.val = value
             if self.head == temp_node:
@@ -104,15 +79,11 @@ class LRUCache:
                 prv = temp_node.prev
                 nxt = temp_node.next
                 if prv:
-                    prv.next = nxt
-                    self.tail = prv
+                    self.update_tail(prv)
                 if nxt:
                     nxt.prev = prv
                 temp_node.prev = None
-                temp_node.next  = self.head
-                self.head.prev = temp_node
-                temp_node.next = self.head
-                self.head = temp_node
+                self.update_head(temp_node)
             else:
                 prv = temp_node.prev
                 nxt = temp_node.next
@@ -121,8 +92,6 @@ class LRUCache:
                 if nxt:
                     nxt.prev = prv
                 temp_node.prev = None
-                temp_node.next  = self.head
-                self.head.prev = temp_node
-                temp_node.next = self.head
-                self.head = temp_node
+                self.update_head(temp_node)
+
         self.map[key] = self.head
