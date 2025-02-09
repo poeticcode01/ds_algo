eadfc23202b7c8364573ccef538f5a519a4fdf2d
poeticcode01 <cmahto01@gmail.com>
2025-02-09 13:23:37 +0530

    buy and sell stock at most k

--- leetcode_blind_75/dp/buy_and_sell_stock_at_most_k.py
+++ leetcode_blind_75/dp/buy_and_sell_stock_at_most_k.py
@@ -3,19 +3,21 @@ from typing import List
 
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        max_transaction_count = k
         days_len = len(prices)
-        dp = [[0]*days_len for i in range(k+1)]
+        dp = [[0]*days_len for i in range(max_transaction_count+1)]
+        
 
-        for i in range(1,k+1):
-            for j in range(1,days_len):
-                dp[i][j] = max(dp[i][j-1],self.sell_stock(i,j,dp,prices))
+        for transaction_so_far in range(1,max_transaction_count+1):
+            for days_so_far in range(1,days_len):
+                dp[transaction_so_far][days_so_far ] = max(dp[transaction_so_far][days_so_far-1],self.sell_stock(transaction_so_far,days_so_far,dp,prices))
 
         return dp[k][days_len-1]
 
-    def sell_stock(self,i,j,dp,prices):
+    def sell_stock(self,transaction_so_far,days_so_far ,dp,prices):
         max_profit = 0
-        for day in range(j):
-            sold_price = prices[j] - prices[day]
+        for day in range(days_so_far):
+            sold_price = prices[days_so_far] - prices[day]
             if sold_price > 0:
-                max_profit = max(max_profit,dp[i-1][day]+sold_price)
+                max_profit = max(max_profit,dp[transaction_so_far-1][day]+sold_price)
         return max_profit
