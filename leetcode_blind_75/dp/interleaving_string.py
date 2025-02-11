e0ee984944e9baf604f815921e93d8217368bbee
poeticcode01 <cmahto01@gmail.com>
2025-02-11 22:09:44 +0530

    redo interleaving string

--- leetcode_blind_75/dp/interleaving_string.py
+++ leetcode_blind_75/dp/interleaving_string.py
@@ -2,40 +2,29 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         s1_len = len(s1)
         s2_len = len(s2)
-
         s3_len = len(s3)
 
         if s1_len + s2_len != s3_len:
             return False
 
-        dp = []
-        for i in range(s1_len+1):
-            temp = []
-            for j in range(s2_len+1):
-                temp.append(False)
-            dp.append(temp)
-
-        # return dp
-
+        dp = [[False]*(s2_len+1) for i in range(s1_len+1)]
+        dp[0][0] = True
+        
         for i in range(s1_len+1):
             for j in range(s2_len+1):
-                if i == j and i == 0:
-                    dp[i][j] = True
+                if i == 0 and j == 0:
                     continue
-
                 if i == 0:
-                    dp[i][j] = dp[i][j-1] and (s2[j-1]== s3[j-1])
+                    dp[i][j] = dp[i][j-1] and (s3[j-1] == s2[j-1])
                     continue
                 if j == 0:
-                    dp[i][j] = dp[i-1][j] and (s1[i-1] == s3[i-1])
+                    dp[i][j] = dp[i-1][j] and (s3[i-1] == s1[i-1])
                     continue
 
                 if s1[i-1] == s3[i+j-1]:
                     dp[i][j] = dp[i][j] or dp[i-1][j]
 
-                if s2[j-1] == s3[i+j-1] :
-                    dp[i][j] =  dp[i][j] or dp[i][j-1]
+                if s2[j-1] == s3[i+j-1]:
+                    dp[i][j] = dp[i][j] or dp[i][j-1]
 
-                
-        # print(dp)
         return dp[s1_len][s2_len]
