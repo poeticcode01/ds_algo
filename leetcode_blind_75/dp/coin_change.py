c0cb4d7f64c1753bb02fd7b851c8795280392baf
poeticcode01 <cmahto01@gmail.com>
2025-02-14 21:10:10 +0530

    coin change

--- leetcode_blind_75/dp/coin_change.py
+++ leetcode_blind_75/dp/coin_change.py
@@ -1,29 +1,47 @@
 from typing import List
 
-
+# Approach 1
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [None]*(amount+1)
-        return self.mincoin(amount,coins,dp)
-
-
-    def mincoin(self,amount,coins,dp):
-        if amount == 0:
-            return 0
-        if dp[amount] is not None:
-            return dp[amount]
-        min_denom = -1
-        for coin in coins:
-            if amount - coin < 0:
-                continue
-            temp = self.mincoin(amount-coin,coins,dp)
-            if temp == -1:
-                continue
-            ans = 1 + temp
-            if min_denom == -1:
-                min_denom = ans
-            else:
-                min_denom = min(min_denom,ans)
+        coins = sorted(coins)
+        dp = [-1 for i in range(amount+1)]
+        dp[0] = 0
+        for i in range(1,amount+1):
+            for coin in coins:
+                if coin <= i:
+                    temp = dp[i-coin]
+                    if temp != -1:
+                        if dp[i] != -1:
+                            dp[i] = min(dp[i],temp+1)
+                        else:
+                            dp[i] = temp+1
         
-        dp[amount] = min_denom
-        return min_denom
+        return dp[amount]
+
+# Approach 2
+# class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         dp = [None]*(amount+1)
+#         return self.mincoin(amount,coins,dp)
+
+
+#     def mincoin(self,amount,coins,dp):
+#         if amount == 0:
+#             return 0
+#         if dp[amount] is not None:
+#             return dp[amount]
+#         min_denom = -1
+#         for coin in coins:
+#             if amount - coin < 0:
+#                 continue
+#             temp = self.mincoin(amount-coin,coins,dp)
+#             if temp == -1:
+#                 continue
+#             ans = 1 + temp
+#             if min_denom == -1:
+#                 min_denom = ans
+#             else:
+#                 min_denom = min(min_denom,ans)
+        
+#         dp[amount] = min_denom
+#         return min_denom
