042d061f6e18f8dc675260f524c7a1bf109f2034
poeticcode01 <cmahto01@gmail.com>
2025-02-15 19:50:48 +0530

    min path sum

--- leetcode_blind_75/dp/min_path_sum.py
+++ leetcode_blind_75/dp/min_path_sum.py
@@ -6,29 +6,27 @@ class Solution:
         row = len(grid)
         col = len(grid[0])
 
-        dp = []
-        for i in range(row):
-            temp = []
-            for j in range(col):
-                temp.append(grid[i][j])
-            dp.append(temp)
+        dp = [[float('inf')]*col for i in range(row)]
+        dp[row-1][col-1] = grid[row-1][col-1]
 
-        # print(dp)
-        cur_col = col - 1
+        i = row-2
+        while i >= 0:
+            dp[i][col-1] = grid[i][col-1] + dp[i+1][col-1]
+            i -=1
+
+        i = col-2
+        while i >= 0:
+            dp[row-1][i] = grid[row-1][i] + dp[row-1][i+1]
+            i -=1
+
+        cur_col = col-2
         while cur_col >= 0:
-            cur_row = row - 1
+            cur_row = row-2
             while cur_row >= 0:
-                right = float('inf')
-                down = float('inf')
-                if cur_col + 1 < col:
-                    right = dp[cur_row][cur_col+1]
-                if cur_row + 1 < row:
-                    down = dp[cur_row+1][cur_col]
-                if right == down and right == float('inf'):
-                    cur_row -=1
-                    continue
-                dp[cur_row][cur_col] = min(right,down) + dp[cur_row][cur_col]
+                dp[cur_row][cur_col] = grid[cur_row][cur_col] + min(dp[cur_row][cur_col+1],dp[cur_row+1][cur_col])
                 cur_row -=1
             cur_col -=1
-        # print(dp)
+        
         return dp[0][0]
+        
+        
