9fc4ea43d21332f6c92dba79bc66e4dae320eec8
poeticcode01 <cmahto01@gmail.com>
2025-02-18 21:24:09 +0530

    min edit distance

--- leetcode_blind_75/dp/min_edit_distance.py
+++ leetcode_blind_75/dp/min_edit_distance.py
@@ -2,18 +2,21 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         word1_len = len(word1)
         word2_len = len(word2)
+
         dp = [[0]*(word2_len+1) for i in range(word1_len+1)]
 
-        for i in range(word2_len+1):
-            dp[0][i] = i
         for i in range(word1_len+1):
-            dp[i][0] = i
+            for j in range(word2_len+1):
+                if i == 0:
+                    dp[i][j] = j
+                    continue
+                if j == 0:
+                    dp[i][j] = i
+                    continue
 
-        for i in range(1,word1_len+1):
-            for j in range(1,word2_len+1):
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1])+1
-
-        return dp[word1_len][word2_len]
+        
+        return dp[word1_len][word2_len] 
