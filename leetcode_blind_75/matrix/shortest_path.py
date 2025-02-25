24b3b02e8565a8d6e424515620650c4242d077a5
poeticcode01 <cmahto01@gmail.com>
2025-02-25 20:42:35 +0530

    final code for shortest path

--- leetcode_blind_75/matrix/shortest_path.py
+++ leetcode_blind_75/matrix/shortest_path.py
@@ -1,60 +1,29 @@
+from collections import deque
+from typing import List
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        obstacle_list = []
+        
 
         row_cnt = len(grid)
         col_cnt = len(grid[0])
 
-        for cur_row   in range(row_cnt):
-            for cur_col in range(col_cnt):
-                if grid[cur_row][cur_col] == 1:
-                    obstacle_list.append((cur_row,cur_col))
+        if k >= (row_cnt-1) + (col_cnt-1):
+            return row_cnt+col_cnt-2
 
-        dp = [float('inf')*col_cnt for i in range(row_cnt)]
-        ans = float('inf')
-        for cur_k in range(k):
-            ans_set = [] 
-            self.getAllKlenObstacles(obstacle_list,0,set(),cur_k,ans_set)
-            for cur_set in ans_set:
-                
-                if grid[row_cnt-1][col_cnt-1] == 0:
-                    dp[row_cnt-1][col_cnt-1] = 0
-                else:
-                    if (row_cnt-1,col_cnt-1) in cur_set:
-                        dp[row_cnt-1][col_cnt-1] = 0
-                col = cur_col - 1
-                i = row -2
-                while i >= 0:
-                    if grid[i][col] == 1:
-                        if (i,col) in cur_set:
-                            dp[i][col] = 1 + dp[i+1][col]
-                    else:
-                        dp[i][col] = 1 + dp[i+1][col]
-                    i -=1
-                
-
-
-                
-                    
-
-
-
-
-
-
-        
-    def reset_dp(self):
-        pass
-
-    def getAllKlenObstacles(self,obstacles,offset,run_set,k,ans_set):
-        if len(run_set) == k:
-            ans_set.append(run_set)
-            return
-
-        temp = offset
-        while temp < len(obstacles):
-            new_set = run_set.union({obstacles[temp]})
-            if len(new_set) <= k:
-                self.getAllKlenObstacles(obstacles,temp+1,new_set,k,ans_set)
-            self.getAllKlenObstacles(obstacles,temp+1,run_set,k,ans_set)
-            temp +=1
+        dq = deque([(0,(0,0,k))])
+        visited = set((0,0,k))
+        directions = [(1,0),(-1,0),(0,-1),(0,1)]
+        while dq:
+            steps_taken,(cur_row,cur_col,remaining_eliminate) = dq.popleft()
+            if cur_row == row_cnt-1 and cur_col == col_cnt-1:
+                return steps_taken
+            
+            for row_increment , col_increment in directions:
+                new_row = cur_row+row_increment
+                new_col = cur_col+col_increment
+                if (new_row >= 0 and new_row < row_cnt) and (new_col >= 0 and new_col < col_cnt):
+                    new_remaining = remaining_eliminate - grid[new_row][new_col]
+                    if new_remaining >= 0 and (new_row,new_col,new_remaining) not in visited:
+                        visited.add((new_row,new_col,new_remaining))
+                        dq.append((steps_taken+1,(new_row,new_col,new_remaining)))
+        return -1
