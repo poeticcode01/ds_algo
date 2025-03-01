e7dd95a99a9b6bd67d2750181f89f974695e04ec
poeticcode01 <cmahto01@gmail.com>
2025-03-01 20:40:23 +0530

    redo basic calculator

--- leetcode_blind_75/stack/basic_calculator.py
+++ leetcode_blind_75/stack/basic_calculator.py
@@ -1,24 +1,28 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        cur=res=0
+        sign=1
         stck = []
-        res=cur=0
-        sign = 1
         for char in s:
             if char.isdigit():
                 cur = cur*10 + int(char)
             elif char in ["+","-"]:
-                res = res + sign*cur
+                # print(type(res),type(sign),type(cur))
+                res =  res + (sign*cur)
                 sign = 1 if char == "+" else -1
                 cur = 0
             elif char == "(":
                 stck.append(res)
                 stck.append(sign)
                 res = 0
-                sign = 1
-            elif char == ")":
-                res += sign*cur
-                res *= stck.pop()
-                res += stck.pop()
                 cur = 0
                 sign = 1
-        return res + sign*cur
+            elif char == ")":
+                res = res + sign*cur
+                sign = stck.pop()
+                res = sign*res
+                prev = stck.pop()
+                res = res + prev
+                cur = 0 
+                sign =1
+        return res + sign*int(cur)
