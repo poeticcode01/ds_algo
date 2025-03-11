d26611d614b4e82d3656406507b4799977d80d38
poeticcode01 <cmahto01@gmail.com>
2025-03-11 19:45:21 +0530

    simplify path

--- leetcode_blind_75/stack/simplify_path.py
+++ leetcode_blind_75/stack/simplify_path.py
@@ -1,27 +1,18 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         path_list = path.split("/")
-        simple_path = []
         stck = []
-        special_char_set = set([".","..","..."])
-        # print(path_list)
-        for ind, itm in enumerate(path_list):
+        special_char_set = set([".",".."])
+        for itm in path_list:
             if itm == "":
                 continue
-
             if itm in special_char_set:
                 if itm == ".":
                     continue
-                elif itm == "..":
+                else:
                     if stck:
                         stck.pop()
-                else:
-                    stck.append(itm)
             else:
                 stck.append(itm)
-
-        # print(stck)
-        ans_str = "/".join(stck)
-        ans_str = "/"+ans_str
-
-        return ans_str
+                
+        return "/"+"/".join(stck)
