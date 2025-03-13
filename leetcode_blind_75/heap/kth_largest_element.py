b811eb963846562d8cdfe006ca4022d0b57c754e
poeticcode01 <cmahto01@gmail.com>
2025-03-13 20:59:15 +0530

    kth largest element

--- leetcode_blind_75/heap/kth_largest_element.py
+++ leetcode_blind_75/heap/kth_largest_element.py
@@ -2,15 +2,13 @@ import heapq
 from typing import List
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums_len = len(nums)
         min_heap = []
         for i in range(k):
             heapq.heappush(min_heap,nums[i])
 
-        for i in range(k,nums_len):
-            top_element = min_heap[0]
-            if nums[i] > top_element:
-                pop_element = heapq.heappop(min_heap)
+        for i in range(k,len(nums)):
+            if nums[i] > min_heap[0]:
+                heapq.heappop(min_heap)
                 heapq.heappush(min_heap,nums[i])
-                
+
         return min_heap[0]
